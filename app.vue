3d25467fb3b33bdd91a327353c3ccc22e70e02c8
Miranda <joaomiranda@fastmail.com>
2023-02-12 21:02:20 +0000

    removed unused stylesg

--- app.vue
+++ app.vue
@@ -17,14 +17,4 @@ body {
   @apply h-full bg-light-800 dark:bg-dark-800 text-dark-800 dark:text-light-800;
   transition: background-color 0.4s, color 0.4s;
 }
-
-// .page-enter-active,
-// .page-leave-active {
-//   transition: all 0.2s;
-// }
-// .page-enter-from,
-// .page-leave-to {
-//   opacity: 0;
-//   filter: blur(1rem);
-// }
 </style>
